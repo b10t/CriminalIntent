23fcf1a77d9939a72500799390b4685b747e9525
b10t <b10t.rus@gmail.com>
2018-05-24 15:55:33 +0800

    300

--- app/src/main/java/ru/fintrans/ui/criminalintent/CrimeListFragment.java
+++ app/src/main/java/ru/fintrans/ui/criminalintent/CrimeListFragment.java
@@ -125,6 +125,7 @@ public class CrimeListFragment extends Fragment {
             mAdapter = new CrimeAdapter(crimes);
             mCrimeRecyclerView.setAdapter(mAdapter);
         } else {
+            mAdapter.setCrimes(crimes);
             mAdapter.notifyDataSetChanged();
         }
 
@@ -190,5 +191,8 @@ public class CrimeListFragment extends Fragment {
             return mCrimes.size();
         }
 
+        public void setCrimes(List<Crime> crimes) {
+            mCrimes = crimes;
+        }
     }
 }
