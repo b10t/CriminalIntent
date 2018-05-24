e658094808124dcbc64d3a322ed713b709df1f09
b10t <b10t.rus@gmail.com>
2018-05-24 11:53:20 +0800

    296

--- app/src/main/java/ru/fintrans/ui/criminalintent/CrimeLab.java
+++ app/src/main/java/ru/fintrans/ui/criminalintent/CrimeLab.java
@@ -1,6 +1,8 @@
 package ru.fintrans.ui.criminalintent;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import java.util.ArrayList;
@@ -8,10 +10,11 @@ import java.util.List;
 import java.util.UUID;
 
 import database.CrimeBaseHelper;
+import database.CrimeDbSchema.CrimeTable;
 
 public class CrimeLab {
     private static CrimeLab sCrimeLab;
-    private List<Crime> mCrimes;
+
     private Context mContext;
     private SQLiteDatabase mDatabase;
 
@@ -28,24 +31,55 @@ public class CrimeLab {
         mDatabase = new CrimeBaseHelper(mContext)
                 .getWritableDatabase();
 
-        mCrimes = new ArrayList<>();
+
     }
 
     public List<Crime> getCrimes() {
-        return mCrimes;
+        return new ArrayList<>();
     }
 
     public Crime getCrime(UUID id) {
-        for (Crime crime : mCrimes) {
-            if (crime.getId().equals(id)) {
-                return crime;
-            }
-        }
 
         return null;
     }
 
     public void addCrime(Crime c) {
-        mCrimes.add(c);
+        ContentValues values = getContentValues(c);
+
+        mDatabase.insert(CrimeTable.NAME, null, values);
+    }
+
+    private static ContentValues getContentValues(Crime crime) {
+        ContentValues values = new ContentValues();
+
+        values.put(CrimeTable.Cols.UUID, crime.getId().toString());
+        values.put(CrimeTable.Cols.TITLE, crime.getTitle());
+        values.put(CrimeTable.Cols.DATE, crime.getDate().getTime());
+        values.put(CrimeTable.Cols.SOLVED, crime.isSolved() ? 1 : 0);
+
+        return values;
+    }
+
+    public void updateCrime(Crime crime) {
+        String uuidString = crime.getId().toString();
+        ContentValues values = getContentValues(crime);
+
+        mDatabase.update(CrimeTable.NAME, values,
+                CrimeTable.Cols.UUID + " = ?",
+                new String[] { uuidString } );
+    }
+
+    private Cursor queryCrimes(String whereClause, String[] whereArgs) {
+        Cursor cursor = mDatabase.query(
+                CrimeTable.NAME,
+                null, // colums = null - выбираются все столбцы
+                whereClause,
+                whereArgs,
+                null, // groupBy
+                null,
+                null
+        );
+
+        return cursor;
     }
 }
