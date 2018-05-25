162fb534ea11852493d2c50d6a78058b755df1a1
b10t <b10t.rus@gmail.com>
2018-05-25 14:44:19 +0800

    test branch

--- app/src/main/java/ru/fintrans/ui/criminalintent/CrimeFragment.java
+++ app/src/main/java/ru/fintrans/ui/criminalintent/CrimeFragment.java
@@ -200,6 +200,13 @@ public class CrimeFragment extends Fragment {
         });
 
         mPhotoView = (ImageView) v.findViewById(R.id.crime_photo);
+        mPhotoView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
         updatephotoView();
 
         return v;
